f9e2265dd0994e04e2ccf7e31167093ffb4427e0
RohanSkaria <rohan.skaria@icloud.com>
2025-02-02 13:37:11 -0500

    feat: configured views and added floating button.

--- app/src/main/java/edu/northeastern/numad25sp_rohanskaria/LinkCollectorActivity.java
+++ app/src/main/java/edu/northeastern/numad25sp_rohanskaria/LinkCollectorActivity.java
@@ -1,4 +1,26 @@
 package edu.northeastern.numad25sp_rohanskaria;
 
-public class LinkCollectorActivity {
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+public class LinkCollectorActivity extends AppCompatActivity {
+    private RecyclerView rv;
+    private FloatingActionButton fab;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_links);
+
+        rv = findViewById(R.id.rv);
+        fab = findViewById(R.id.fab);
+
+        fab.setOnClickListener(view -> {
+            System.out.println("Button Clicked");
+        });
+    }
 }
