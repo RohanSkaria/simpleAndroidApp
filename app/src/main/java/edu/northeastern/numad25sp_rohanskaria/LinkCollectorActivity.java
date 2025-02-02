b453ccc9080f3b77ecaf5d4bacfffb88a1fd7855
RohanSkaria <rohan.skaria@icloud.com>
2025-02-02 18:26:13 -0500

    feat: added proper dialog page when adding link along with styling.

--- app/src/main/java/edu/northeastern/numad25sp_rohanskaria/LinkCollectorActivity.java
+++ app/src/main/java/edu/northeastern/numad25sp_rohanskaria/LinkCollectorActivity.java
@@ -1,12 +1,17 @@
 package edu.northeastern.numad25sp_rohanskaria;
 
+import android.app.AlertDialog;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.EditText;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
 
 public class LinkCollectorActivity extends AppCompatActivity {
     private RecyclerView rv;
@@ -28,5 +33,23 @@ public class LinkCollectorActivity extends AppCompatActivity {
 
         adapter = new LinkAdapter();
         rv.setAdapter(adapter);
+    }
+
+    private void showAddLinkMessage() {
+        View dialogView = LayoutInflater.from(this).inflate(R.layout.add_link_card, null);
+        EditText nameEdit = dialogView.findViewById(R.id.editName);
+        EditText urlEdit = dialogView.findViewById(R.id.editUrl);
+
+        new AlertDialog.Builder(this).setTitle("Add Link").setView(dialogView)
+                .setPositiveButton("Add", (dialog, which) -> {
+                    String name = nameEdit.getText().toString();
+                    String url = urlEdit.getText().toString();
+                    Link newLink = new Link(name, url);
+                    adapter.addLink(newLink);
+
+                Snackbar.make(rv, "Link Added", Snackbar.LENGTH_LONG).show();
+                })
+                .setNegativeButton("Cancel",null)
+                .show();
     }
 }
