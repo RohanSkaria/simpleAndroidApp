9bb73bdd88e510028beb535d305e04e16cce1c57
RohanSkaria <rohan.skaria@icloud.com>
2025-02-02 18:10:30 -0500

    feat: created an adapter to display links.

---
+++ app/src/main/java/edu/northeastern/numad25sp_rohanskaria/LinkAdapter.java
@@ -0,0 +1,51 @@
+package edu.northeastern.numad25sp_rohanskaria;
+
+import androidx.recyclerview.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+import androidx.annotation.NonNull;
+import java.util.ArrayList;
+public class LinkAdapter extends RecyclerView.Adapter<LinkAdapter.LinkViewHolder> {
+
+    private ArrayList<Link> links;
+
+    public LinkAdapter() {
+        this.links = new ArrayList<>();
+    }
+
+    @NonNull
+    @Override
+    public LinkViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.link_card,parent,false);
+        return new LinkViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull LinkViewHolder holder, int position) {
+        Link currentLink = links.get(position);
+        holder.name.setText(currentLink.getName());
+    }
+
+    @Override
+    public int getItemCount() {
+        return links.size();
+    }
+
+    public void addLink(Link link) {
+        links.add(link);
+        notifyItemInserted(links.size() -1);
+    }
+
+    static class LinkViewHolder extends RecyclerView.ViewHolder {
+        TextView name;
+        TextView url;
+
+        LinkViewHolder(View link) {
+            super(link);
+            name = link.findViewById(R.id.linkName);
+            url = link.findViewById(R.id.linkUrl);
+        }
+    }
+}
--- app/src/main/java/edu/northeastern/numad25sp_rohanskaria/LinkCollectorActivity.java
+++ app/src/main/java/edu/northeastern/numad25sp_rohanskaria/LinkCollectorActivity.java
@@ -3,6 +3,7 @@ package edu.northeastern.numad25sp_rohanskaria;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
@@ -10,6 +11,7 @@ import com.google.android.material.floatingactionbutton.FloatingActionButton;
 public class LinkCollectorActivity extends AppCompatActivity {
     private RecyclerView rv;
     private FloatingActionButton fab;
+    private LinkAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,10 +19,14 @@ public class LinkCollectorActivity extends AppCompatActivity {
         setContentView(R.layout.activity_links);
 
         rv = findViewById(R.id.rv);
-        fab = findViewById(R.id.fab);
+        rv.setLayoutManager(new LinearLayoutManager(this));
 
+        fab = findViewById(R.id.fab);
         fab.setOnClickListener(view -> {
             System.out.println("Button Clicked");
         });
+
+        adapter = new LinkAdapter();
+        rv.setAdapter(adapter);
     }
 }
