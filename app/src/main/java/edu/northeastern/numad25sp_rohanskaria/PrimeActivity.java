67a498372c3e33a0a684be4730485d73d6db97bd
RohanSkaria <rohan.skaria@icloud.com>
2025-02-09 15:45:33 -0500

    feat: updated Activity with components from xml.

--- app/src/main/java/edu/northeastern/numad25sp_rohanskaria/PrimeActivity.java
+++ app/src/main/java/edu/northeastern/numad25sp_rohanskaria/PrimeActivity.java
@@ -1,4 +1,27 @@
 package edu.northeastern.numad25sp_rohanskaria;
 
-public class PrimeActivity {
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.TextView;
+import androidx.appcompat.app.AppCompatActivity;
+public class PrimeActivity extends AppCompatActivity{
+    private Button findPrimesButton;
+    private Button terminateSearchButton;
+    private CheckBox pacifierSwitchCheck;
+    private TextView currNumberText;
+    private TextView lastPrimeNumberText;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_prime);
+        findPrimesButton = findViewById(R.id.findPrimesButton);
+        terminateSearchButton = findViewById(R.id.terminateSearchButton);
+        pacifierSwitchCheck = findViewById(R.id.pacifierSwitchCheck);
+        currNumberText = findViewById(R.id.currNumberText);
+        lastPrimeNumberText = findViewById(R.id.lastPrimeNumberText);
+    }
+
 }
